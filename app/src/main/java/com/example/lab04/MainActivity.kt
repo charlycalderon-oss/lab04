87e1019d36e67f1536c5168f91dab614ecf618bd
Charly <mrganzotc@gmail.com>
2025-09-20 00:39:56 -0500

    feat: Estilos actualizados para WelcomeText

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -18,7 +18,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.lab04.ui.theme.Lab04Theme
 
 class MainActivity : ComponentActivity() {
@@ -61,8 +63,13 @@ fun ButtonExample() {
 
 @Composable
 fun WelcomeText() {
-    Text(text = "¡Bienvenido al laboratorio 04!")
+    Text(
+        text = "¡Bienvenido al laboratorio 04!",
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold
+    )
 }
+
 
 
 @Preview(showBackground = true)
